52155ba6211f17a98595ccd8fd091e2893fdb8f8
Nice <vorason.si@gmail.com>
2019-01-12 22:24:52 +0700

    read data informationActivity not finish

--- app/src/main/java/com/example/nice/a491/InformationActivity.java
+++ app/src/main/java/com/example/nice/a491/InformationActivity.java
@@ -3,17 +3,33 @@ package com.example.nice.a491;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class InformationActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myref = database.getReference("0001/patient");
+    private TextView idtv, hospitalidtv, desidtv, diseasetv, sextv, statustv, agetv;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.information_page);
+        initinstance();
 
     }
 
@@ -37,4 +53,46 @@ public class InformationActivity extends AppCompatActivity {
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
     }
+
+    private void initinstance() {
+
+        myref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+//                Log.d("ggg","kuy" + dataSnapshot.getValue(String.class));
+
+                String age = dataSnapshot.child("age").getValue(String.class);
+                String desid = dataSnapshot.child("desid").getValue(String.class);
+                String disease = dataSnapshot.child("disease").getValue(String.class);
+                String hospitalid = dataSnapshot.child("hospitalid").getValue(String.class);
+                String sex = dataSnapshot.child("sex").getValue(String.class);
+
+                idtv = findViewById(R.id.textView7);
+                hospitalidtv = findViewById(R.id.textView8);
+                agetv = findViewById(R.id.textView9);
+                sextv = findViewById(R.id.textView10);
+                statustv = findViewById(R.id.textView11);
+                desidtv = findViewById(R.id.textView12);
+                diseasetv = findViewById(R.id.textView13);
+
+                idtv.setText("0001");
+                hospitalidtv.setText(hospitalid);
+                agetv.setText(age);
+                sextv.setText(sex);
+                statustv.setText("n");
+                desidtv.setText(desid);
+                diseasetv.setText(disease);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+               Log.d("ggg", "failread realtimedb");
+            }
+
+
+        });
+    }
+
 }
--- app/src/main/java/com/example/nice/a491/MapsActivity.java
+++ app/src/main/java/com/example/nice/a491/MapsActivity.java
@@ -31,10 +31,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -52,7 +49,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private String latitude;
     private String longitude;
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +59,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        addLocation();
+//        addLocation();
+//        getLocation();
     }
 
     /**
@@ -105,7 +103,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         double lat = (location.getLatitude());
         latitude = lat + "";
         longitude = lon + "";
-        Log.i(latitude,longitude);
+        Log.i("read location", latitude + longitude);
+
+
     }
 
     @Override
@@ -153,45 +153,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    private void addLocation() {
-        Map<String, Object> gps = new HashMap<>();
-        gps.put("la", latitude);
-        gps.put("lo", longitude);
-
-
-        // Add a new document with a generated ID
-        db.collection("gps")
-                .add(gps)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("test", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("test", "Error adding document", e);
-                    }
-                });
-    }
-
-//    private void getLocation() {
-//        db.collection("users")
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (DocumentSnapshot document : task.getResult()) {
-//                                Log.d("test", document.getId() + " => " + document.getData());
-//                            }
-//                        } else {
-//                            Log.w("test", "Error getting documents.", task.getException());
-//                        }
-//                    }
-//                });
-//    }
 
 
 }
